b15b3e4f862435d3866a1b5ee67ae42fa706dfce
prod1992 <levon1.grigoryan@gmail.com>
2018-05-08 20:08:16 +0400

    sad

--- api.js
+++ api.js
@@ -13,45 +13,50 @@ api.disable('x-powered-by');
 
 // const url = process.argv[2].replace(/--/, '');
 const Render = async (req, res, next) => {
-    const filename = `receipt_t${new Date().getTime()}.pdf`;
-    const path = `public/${filename}`;
-    const email = req.body.email;
-    const query = req.body.query;
-    const browser = await puppeteer.launch({args: ['--no-sandbox','--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.goto(query, {
-        timeout: 60000,
-        waitUntil: 'domcontentloaded'
-    });
-    await page.type('[name=email]', email);
-    await page.click('[type=submit]');
-    await page.waitForNavigation({
-        timeout: 60000,
-        waitUntil: 'domcontentloaded'
-    });
-    await page.goto(query, {
-        timeout: 60000,
-        waitUntil: 'networkidle0'
-    });
-    await page.pdf({
-        path: path,
-        format: 'A4',
-        printBackground: true,
-        margin: {
-            left: '2.54cm',
-            top: '2.54cm',
-            right: '2.54cm',
-            bottom: '2.54cm'
-        }
-    });
-    res.set({
-        'Content-Type': 'application/pdf',
-        'Content-Size': path.length,
-        'Content-Disposition': contentDisposition(path)
-    });
-    fs.createReadStream(path).pipe(res).on('finish', () => fs.unlink(path, (e) => console.log(e)));
-    next();
-
+    try {
+        const filename = `receipt_t${new Date().getTime()}.pdf`;
+        const path = `public/${filename}`;
+        const email = req.body.email;
+        const query = req.body.query;
+        const browser = await puppeteer.launch({
+            ignoreHTTPSErrors: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.goto(query, {
+            timeout: 60000,
+            waitUntil: 'domcontentloaded'
+        });
+        await page.type('[name=email]', email);
+        await page.click('[type=submit]');
+        await page.waitForNavigation({
+            timeout: 60000,
+            waitUntil: 'domcontentloaded'
+        });
+        await page.goto(query, {
+            timeout: 60000,
+            waitUntil: 'networkidle0'
+        });
+        await page.pdf({
+            path: path,
+            format: 'A4',
+            printBackground: true,
+            margin: {
+                left: '2.54cm',
+                top: '2.54cm',
+                right: '2.54cm',
+                bottom: '2.54cm'
+            }
+        });
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Size': path.length,
+            'Content-Disposition': contentDisposition(path)
+        });
+        fs.createReadStream(path).pipe(res).on('finish', () => fs.unlink(path, (e) => console.log(e)));
+    } catch (e) {
+        next(e);
+    }
 };
 
 api.post('/getpdf', Render);
