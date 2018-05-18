8e967c2c5c92949525f02c95ab0822ba649c44d9
prod1992 <levon1.grigoryan@gmail.com>
2018-05-18 06:56:27 +0400

    updates

--- render.js
+++ render.js
@@ -5,15 +5,14 @@ const filename = `receipt_t${new Date().getTime()}.pdf`;
 const path = `./${filename}`;
 
 
-const render = async (html, callback) => {
+let render = async (html, callback) => {
     const browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
     await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 2});
-    const navigationPromise = page.waitForNavigation({waitUntil: 'load'});
     await page.setContent(html, {waitUntil: 'networkidle0'});
-    await navigationPromise;
+    await page.waitFor(2000);
     await page.pdf({
         path: path,
         format: 'A4',
