b8930f021b38a9308c84d3dde1224adca82c0a88
prod1992 <levon1.grigoryan@gmail.com>
2018-05-18 11:36:26 +0400

    updates

--- api.js
+++ api.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const contentDisposition = require('content-disposition');
 const cors = require('cors');
 const render = require('./render');
-const purifycss = require('purify-css');
 const api = express();
 api.use(bodyParser.json({limit: '10mb'}));
 api.use(bodyParser.urlencoded({extended: false, limit: '10mb'}));
@@ -11,16 +10,6 @@ api.disable('x-powered-by');
 api.use(cors());
 
 api.post('/getpdf', async (req, res, next) => {
-    // const html = await `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-    //             <head>
-    //                 <title>Receipt ${new Date().getTime()}</title>
-    //                 <meta charset="utf-8">
-    //                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    //                 <meta name="viewport" content="width=device-width, initial-scale=1">
-    //                 <style media="all">${req.body.styles}</style>
-    //             </head>
-    //             <body>${req.body.content}</body>
-    //         </html>`;
     const stream = await render(req);
     res.set({
         'Content-Type': 'application/pdf',
