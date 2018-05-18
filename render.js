96e2e8611c7c766bcb33059310cdf9d3550ce5d0
prod1992 <levon1.grigoryan@gmail.com>
2018-05-18 10:36:19 +0400

    updates

--- render.js
+++ render.js
@@ -12,13 +12,19 @@ let render = async (html, req, callback) => {
         headless: true
     });
     const page = await browser.newPage();
+    await page.goto(`data:text/html;charset=UTF-8,${html}`, { waitUntil: 'networkidle0' });
     await page.emulateMedia('screen');
-    await page.goto(`data:text/html,${html}`, { waitUntil: 'networkidle0' });
     await page.pdf({
         path: path,
         format: 'A4',
         printBackground: true,
-        scale: 0.75
+        scale: 0.75,
+        margin: {
+            top: '1cm',
+            bottom: '1cm',
+            left: '1cm',
+            right: '1cm'
+        }
     }).then(callback, (error) => console.error(error));
     await browser.close();
     return fs.createReadStream(path);
