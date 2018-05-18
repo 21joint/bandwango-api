e0782555cd80602d4915ea9880f78ebfe89e4e11
prod1992 <levon1.grigoryan@gmail.com>
2018-05-18 12:19:07 +0400

    updates

--- render.js
+++ render.js
@@ -26,7 +26,13 @@ let render = async (req, callback) => {
         path: path,
         format: 'A4',
         printBackground: true,
-        scale: 0.72
+        scale: 0.72,
+        margin: {
+            top: '0.75cm',
+            bottom: '0.75cm',
+            left: '0.75cm',
+            right: '0.75cm'
+        }
     }).then(callback, (error) => console.error(error));
     await browser.close();
     return fs.createReadStream(path);
