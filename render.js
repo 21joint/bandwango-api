987f9149b7ea1ecfe339a693ac274045a937c84d
prod1992 <levon1.grigoryan@gmail.com>
2018-05-18 08:09:13 +0400

    updates

--- render.js
+++ render.js
@@ -13,7 +13,7 @@ let render = async (html, callback) => {
     await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 2});
     await page.setJavaScriptEnabled(false);
     await page.setContent(html, {waitUntil: 'networkidle0'});
-    await page.waitFor(2000);
+    await page.waitFor(5000);
     await page.pdf({
         path: path,
         format: 'A4',
